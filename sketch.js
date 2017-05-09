cb3be8283b0f8589542c83a5b64a213c899428c4
fermar7 <fermar0715@gmail.com>
2017-05-09 08:46:09 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 blocks = [];
+var ant = null;
 
 function setup() {
     createCanvas(700, 500)
-    frameRate(1);
+    frameRate(3);
+
     var maxH = height / 2;
     for(x = 0; x <= width; x += 5) {
         var col = [];
@@ -12,66 +14,29 @@ function setup() {
         blocks.push(col);
     }
 
+    for(x = 0; x < blocks.length; x++) {
+        for(y = 0; y < blocks[x].length; y++) {
+            blocks[x][y].generate(blocks, x, y, 0.006);
+        }
+    }
+
+    for(x = 0; x < blocks.length; x++) {
+        for(y = 0; y < blocks[x].length; y++) {
+            blocks[x][y].generate(blocks, x, y, 0);
+        }
+    }
+    ant = new Ant(round(random(blocks.length)), round(random(random(blocks[0].length))), blocks);
+
 }
 
 function draw() {
     background(79, 146, 255)
 
-    for(x = 0; x < blocks.length; x += 1) {
-        for(y = 0; y < blocks[x].length; y += 1) {
-            blocks[x][y].generate(blocks, x, y);
-        }
-    }
-
-    for(x = 0; x < blocks.length; x += 1) {
-        for(y = 0; y < blocks[x].length; y += 1) {
+    for(x = 0; x < blocks.length; x++) {
+        for(y = 0; y < blocks[x].length; y++) {
             blocks[x][y].show();
-            blocks[x][y].isStone = false;
         }
     }
-}
 
-function Block(x, y, w, h) {
-    this.pos = createVector(x, y);
-    this.w = w;
-    this.h = h;
-
-    this.isStone = false;
-
-    this.show = function() {
-        if(this.isStone) {
-            fill(51)
-            stroke(51);
-        } else {
-            fill(89, 55, 12)
-            stroke(89, 55, 12);
-        }
-        rect(this.pos.x, this.pos.y, this.w, this.h);
-    }
-
-    this.generate = function(blocks, x, y) {
-        var value = 0.02;
-
-        var n = blocks[x][y - 1];
-        if(n && n.isStone) {
-            value = 0.5;
-        }
-        n = blocks[x][y + 1];
-        if(n && n.isStone) {
-            value = 0.5;
-        }
-        n = (blocks[x - 1] ? blocks[x - 1] : blocks[x])[y];
-        if(n && n.isStone) {
-            value = 0.5;
-        }
-        n = (blocks[x + 1] ? blocks[x + 1] : blocks[x])[y];
-        if(n && n.isStone) {
-            value = 0.5;
-        }
-
-
-        if(random(1) < value) {
-            this.isStone = true;
-        }
-    }
+    ant.show();
 }
